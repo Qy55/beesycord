aa93a8448949a39e9ed602dcd6ef2d917252390e
beesy <mywhqt3h4nd@outlook.com>
2023-05-03 23:01:45 +0900

    command test

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import private
 import discord
+from discord.ext import commands
 
-if __name__ == "__main__":
-    print("init develop")
+
+objIntents = discord.Intents.all()
+objBot = commands.Bot(command_prefix="$", intents = objIntents)
+
+
+
+@objBot.event
+async def on_ready():
+    print("Bot Online")
+    
+
+@objBot.command(aliases = ["안녕"])
+async def hello(ctx):
+    await ctx.send("world")
+    
+@objBot.command(aliases = ["계정신청"])
+async def Register(ctx):
+    pass
+
+objBot.run(private.TOKEN)
+print("init develop")
